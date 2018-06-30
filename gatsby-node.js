6d3b7b110373004826cbfdba939e966bd91ea26c
Lucas Neumann <lucas.neumann@nubank.com.br>
2018-06-30 18:19:46 -0300

    create page from records

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
     const { createNodeField } = boundActionCreators
     if (node.internal.type === 'Airtableprodutos'){
@@ -8,4 +10,32 @@ exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
         value: slug,
     })
     }
+};
+
+exports.createPages = ({graphql, boundActionCreators}) => {
+    const { createPage } = boundActionCreators
+    return new Promise ((resolve, reject) => {
+        graphql(`
+        {allAirtableprodutos{
+            edges{
+            node{
+              fields{
+                slug
+              }
+            }
+          }
+        }}
+    `).then(result => {
+        result.data.allAirtableprodutos.edges.forEach(({ node }) => {
+            createPage({
+                path: node.fields.slug,
+                component: path.resolve(`./src/templates/product.js`),
+                context: {
+                    slug: node.fields.slug
+                },
+            })
+        })
+        resolve()
+    })
+    })
 };
