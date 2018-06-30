0456c9f5fe8812527228368900b095c9ee4ebfa0
Lucas Neumann <lucas.neumann@nubank.com.br>
2018-06-30 18:09:08 -0300

    create slugs

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,11 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
- // You can delete this file if you're not using it
+exports.onCreateNode = ({ node, getNode, boundActionCreators }) => {
+    const { createNodeField } = boundActionCreators
+    if (node.internal.type === 'Airtableprodutos'){
+    const slug = node['Código'];
+    createNodeField({
+        node,
+        name: 'slug',
+        value: slug,
+    })
+    }
+};
